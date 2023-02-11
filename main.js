5c96df9e3a607b5ae801276b485f4fc25435fcb8
bhighlander <saphrimangel@gmail.com>
2023-02-11 10:30:11 -0600

    FIX: fixed merge conflict error

--- main.js
+++ main.js
@@ -83,100 +83,19 @@ window.addEventListener("load", function () {
   }
 });
 
-const footer = () => {
-  const domString = `
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-      <li><h6>2023 Gitsub, Inc.</h6>
-              </li>
-      <li class="nav-item">
-            <a class="nav-link" href="#">Terms</a>
-              <li class="nav-item">
-                 <a class="nav-link" href="#">Privacy</a>
-              </li>
-      <li class="nav-item">
-            <a class="nav-link" href="#">Security</a>
-              </li>
-                <li class="nav-item"></li>
-            <a class="nav-link" href="#">Status</a>
-              </li>
-                <li class="nav-item"></li>
-            <a class="nav-link" href="#">Docs</a>
-              </li>
-                <li class="nav-item"></li>
-            <a class="nav-link" href="#">Contact Gitsub</a>
-              </li>
-                <li class="nav-item"></li>
-            <a class="nav-link" href="#">Pricing</a>
-              </li>
-                <li class="nav-item"></li>
-            <a class="nav-link" href="#">API</a>
-              </li>
-                <li class="nav-item"></li>
-            <a class="nav-link" href="#">Training</a>
-              </li>
-                <li class="nav-item"></li>
-            <a class="nav-link" href="#">Blog</a>
-              </li>
-                <li class="nav-item"></li>
-            <a class="nav-link" href="#">About</a>
-              </li>
-      </ul>
+const repoCardsOnDom = () => {
+  let domString = "";
+  for (const repo of reposData) {
+    domString += `<div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+    <div class="card-header">"${repo.title}"</div>
+    <div class="card-body">
+      <h5 class="card-title">"${repo.tags}"</h5>
+      <p class="card-text">"${repo.description}"</p>
     </div>
-  </div>
-</nav>`;
+  </div>`;
+  }
 
-  renderToDom("#footer", domString);
-};
-
-const startApp = () => {
-  navbar();
-  footer();
-};
-
-startApp();
-=======
-import { packagesData, reposData, projectsData} from "./referenceData.js";
-
-//render to dom function
-const renderToDom = (divId, textToRender) => {
-  const selectedElement = document.querySelector(divId);
-  selectedElement.innerHTML = textToRender;
-};
-
-const navbar = () => {
-  const domString = `
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-      <li class="nav-item">
-      <button type="button" class="btn btn-primary">
-      <a class="nav-link" href="index.html">Overview</a>
-      </button>
-        <li class="nav-item">
-        <button type="button" class="btn btn-primary">
-          <a class="nav-link" href="repositories.html">Repositories</a>
-          </button>
-        </li>
-        <li class="nav-item">
-        <button type="button" class="btn btn-primary">
-          <a class="nav-link" href="projects.html">Projects</a>
-          </button>
-        </li>
-        <li class="nav-item">
-        <button type="button" class="btn btn-primary">
-          <a class="nav-link" href="packages.html">Packages</a>
-          </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>`;
-
-  renderToDom("#navBar", domString);
+  renderToDom("#existingRepos", domString);
 };
 
 const profileCard = () => {
@@ -228,45 +147,7 @@ const profileCard = () => {
   </ul>
 </div>`
 renderToDom('#profile', domString)
-}
-
-const packagesCard = () => {
-  let domString = "";
-  packagesData.forEach((currentPackage) => {
-  domString += `
-  
-  <div class="card">
-    <div class="card-header">
-      Featured
-    </div>
-    <div class="card-body">
-      <h5 class="card-title">${currentPackage.title}</h5>
-      <p class="card-text">${currentPackage.description}.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>`;
-    });
-  renderToDom("#existingPackages", domString);
-  };
-  
-  
-  // loop through repo cards COME BACK TO THIS
-
-const repoCardsOnDom = () => {
-  let domString = "";
-  for (const repo of reposData) {
-    domString += `<div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-    <div class="card-header">"${repo.title}"</div>
-    <div class="card-body">
-      <h5 class="card-title">"${repo.tags}"</h5>
-      <p class="card-text">"${repo.description}"</p>
-    </div>
-  </div>`;
-  }
-
-  renderToDom("#existingRepos", domString);
 };
-
 
 const footer = () => {
   const domString = `
@@ -320,7 +201,6 @@ const startApp = () => {
   navbar();
   profileCard();
   footer();
-  packagesCard();
 };
 
 startApp();
